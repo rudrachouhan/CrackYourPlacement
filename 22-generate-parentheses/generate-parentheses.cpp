318ae821792dd070ae49b9a5753528b050b16472
rudrachouhan <96373185+rudrachouhan@users.noreply.github.com>
2024-07-20 21:33:12 +0530

    Time: 5 ms (31.11%) | Memory: 12 MB (83.32%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
 
-    void solve(int &n,vector<string> &ans,string temp,int open,int close){
+    void solve(int &n,vector<string> &ans,string &temp,int open,int close){
         if(temp.size()==2*n){
             ans.push_back(temp);
             return;
         }
 
         if(open==close){
-            solve(n,ans,temp+'(',open-1,close);
+            temp.push_back('(');
+            solve(n,ans,temp,open-1,close);
+            temp.pop_back();
         }
 
         else{
-            solve(n,ans,temp+')',open,close-1);
+            temp.push_back(')');
+            solve(n,ans,temp,open,close-1);
+            temp.pop_back();
             if(open>0){
-                solve(n,ans,temp+'(',open-1,close);
+                temp.push_back('(');
+                solve(n,ans,temp,open-1,close);
+                temp.pop_back();
             }
         }
     }
