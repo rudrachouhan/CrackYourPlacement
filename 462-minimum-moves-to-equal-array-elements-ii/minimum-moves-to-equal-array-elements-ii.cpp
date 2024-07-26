fe36c12acf5555763f445bd59eeaac5d9009c3ef
rudrachouhan <96373185+rudrachouhan@users.noreply.github.com>
2024-07-26 18:06:00 +0530

    Time: 10 ms (43.14%) | Memory: 14.8 MB (34.71%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        long long int ans = 0,n=nums.size();
-        sort(nums.begin(),nums.end());
-        unordered_map<int,int> mp;
-        int a = nums[0],b=nums[n-1];
-        int mid = a + (b-a)/2;
-        for(int i=0;i<n;i++){
-            ans+=abs(nums[i]-mid);
-            mp[nums[i]]++;
-        }
-        long long int res = INT_MAX;
-        for(auto i:mp){
-            int a = i.first;
-            long long int count=0;
-            for(auto j:mp){
-                count+=(j.second*abs(j.first-a));
-            }
-            res = min(res,count);
-        }
-        return min(ans,res);
+        sort(nums.begin(), nums.end());
+        int mid = nums[nums.size()/2];
+        int ans = 0;
+        for(auto val:nums)
+            ans += abs(mid - val);
+        return ans;
     }
 };
